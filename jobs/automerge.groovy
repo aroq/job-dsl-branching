8808d9c8feb0b460106b89926b77ad7ffa42286e
Alexander Tolstikov <atolstikov@adyax.com>
2016-02-15 18:41:34 +0300

    Environment detection

--- jobs/automerge.groovy
+++ jobs/automerge.groovy
@@ -5,23 +5,28 @@ import com.github.aroq.groovycommon.Config
 config = Config.instance.config
 
 Config.instance.addParams(System.properties)
+configFilePath = 'config/config.groovy'
+
+def conf
+
+config.rootDir = Common.instance.rootDir()
 
 // Determine current environment.
 try {
     BUILD_NUMBER
     config.environment = 'jenkins'
+    conf = readFileFromWorkspace(configFilePath)
 } catch (MissingPropertyException mpe) {
     config.environment = 'local'
-    def configFile = new File(config.rootDir + '/config/config.groovy')
-
+    conf = new File(config.rootDir, configFilePath).text
 }
 
-config.rootDir = Common.instance.rootDir()
+Config.instance.addParams(ConfigSlurper.newInstance(config.environment).parse(conf))
+
 println config.environment
 println config.rootDir
 println System.properties
 
-Config.instance.addParams(ConfigSlurper.newInstance(config.environment).parse(configFile.text))
 
 (new AutoMergeJobDSLCreator(componentName: 'automerge', scriptObject: this)).execute()
 
